db5dc8be3e02f0558f8035c4139240bb807bc586
unknown <ronak@Kuntal.promact.com>
2015-06-18 15:43:25 +0530

    some commit

---
+++ MVCronak/Controllers/helloworldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCronak.Controllers
+{
+    public class helloworldController : Controller
+    {
+        // GET: helloworld
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult  welcome(String name,int numtimes=1)
+        {
+            ViewBag.Message = "hello" + name;
+            ViewBag.Numtimes = numtimes;
+            return View();
+        }
+    }
+}
